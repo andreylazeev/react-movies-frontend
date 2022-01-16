b47e5413d0b4d47a75fc480766028eacc2587607
andeylazeev <lazeev1998@yandex.ru>
2022-01-16 17:35:45 +0700

    add inputs to modal

--- src/components/AuthModal/AuthModal.tsx
+++ src/components/AuthModal/AuthModal.tsx
@@ -1,17 +1,25 @@
-import { FC, useEffect, useRef } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { useRecoilState } from 'recoil'
+import { useInput } from '../../hooks/input.hook'
 import { store } from '../../recoil'
 
 import './AuthModal.scss'
 
-export const AuthModal:FC = () => {
+export const AuthModal: FC = () => {
   const [state, setState] = useRecoilState(store)
+  const [currentTab, setCurrentTab] = useState(0)
   const ref = useRef<HTMLDivElement>(null)
+  const username = useInput('')
+  const password = useInput('')
 
   const handleOutsideClick = (e: MouseEvent) => {
     if (!ref.current?.contains(e.target as HTMLElement)) {
-      setState(prev => ({...prev, isModalVisible: false}));
+      setState((prev) => ({ ...prev, isModalVisible: false }))
     }
+  }
+
+  const switchTabs = (value: number) => {
+    setCurrentTab(value)
   }
 
   useEffect(() => {
@@ -21,8 +29,30 @@ export const AuthModal:FC = () => {
     }
   }, [])
   return (
-    <div className='AuthModal' ref={ref}>
-      modal
+    <div className='AuthModal'>
+      <div className='AuthModal__Wrap' ref={ref}>
+        <div className='AuthModal__Tabs'>
+          <div
+            className={`AuthModal__Tab ${currentTab === 0 ? 'AuthModal__Tab--Active' : ''}`}
+            onClick={() => switchTabs(0)}
+          >
+            Вход
+          </div>
+          <div
+            className={`AuthModal__Tab ${currentTab === 1 ? 'AuthModal__Tab--Active' : ''}`}
+            onClick={() => switchTabs(1)}
+          >
+            Регистрация
+          </div>
+        </div>
+        <div className='AuthModal__Inputs'>
+          <input type='text' placeholder='Имя пользователя' className='AuthModal__Input' {...username} />
+          <input type='text' placeholder='Пароль' className='AuthModal__Input' {...password} />
+        </div>
+        <div className="AuthModal__Button">
+          <button className="Navbar__Login">Вход/Регистрация</button>
+        </div>
+      </div>
     </div>
   )
 }
